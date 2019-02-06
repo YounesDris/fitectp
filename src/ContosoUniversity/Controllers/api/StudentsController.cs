d24631d531e1825487442142dc0ab8bd20d4e180
Clément Guittet <clement.guit@gmail.com>
2019-02-06 18:02:33 +0100

    Api_Etudiant

---
+++ src/ContosoUniversity/Controllers/api/StudentsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ContosoUniversity.DAL;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Controllers.api
+{
+    public class StudentsController : ApiController
+    {
+        private SchoolContext db = new SchoolContext();
+
+        // GET: api/Students
+        public IQueryable<Student> GetPeople()
+        {
+            return db.Students;
+        }
+
+        // GET: api/Students/5
+        [ResponseType(typeof(Student))]
+        public IHttpActionResult GetStudent(int id)
+        {
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(student);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool StudentExists(int id)
+        {
+            return db.People.Count(e => e.ID == id) > 0;
+        }
+    }
+}
