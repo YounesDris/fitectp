9d853aaac852b45a723a6ef8d4a1f5adaf7db535
Younes DRIS <younes.dris@gmail.com>
2019-02-07 14:00:51 +0100

    V1 Stable Reste Tests à effectuer

--- src/ContosoUniversity/Controllers/SubscribeController.cs
+++ src/ContosoUniversity/Controllers/SubscribeController.cs
@@ -14,12 +14,22 @@ namespace ContosoUniversity.Controllers
 
         public ActionResult SubscribeCourse(int? id)
         {
+            // Selectionner l'ensemble des cours dispensés au sein de l'université.
             var query1 = db.Courses.Select(c => c.CourseID);
-            var query2 = db.Enrollments.Where(s => s.StudentID == id).Select(s => s.CourseID);
-            var finalquery = query1.Except(query2);
-            
-            ViewBag.ApplyCourse = finalquery;
 
+            // Selectionner l'ensemble des cours auxquels l'étudiant est inscrit.
+            var query2 = db.Enrollments.Where(s => s.StudentID == id).Select(s => s.CourseID);
+
+            // Selectionner l'ensemble des cours auxquels l'étudiant peut postuler.
+            var finalquery = query1.Except(query2);
+
+            // Selectionner l'ensemble des cours et leurs libellés auxquels l'étudiant peut postuler. 
+            var available = db.Courses.Where(c => finalquery.Contains(c.CourseID)).Select(c => new { c.CourseID, c.Title });
+
+            // Ce ViewBag permet de partager les cours et leurs libellés du contrôleur à la vue.
+            ViewBag.ApplyCourse = available;
+
+            // Ce tempData permet de passer l'ID à l'action suivante. 
             TempData["studentID"] = id;
 
             return View();
@@ -36,7 +46,7 @@ namespace ContosoUniversity.Controllers
 
             int id = int.Parse(TempData["studentID"].ToString());
 
-            db.Enrollments.Add(new Enrollment { StudentID = id, CourseID = courseID, Grade = grade });
+            db.Enrollments.Add(new Enrollment { StudentID = id, CourseID = courseID });
             db.SaveChanges();
             ViewBag.Message = "Subscription successful !";
             return RedirectToAction("SessionStudent", "Login", new { id });
